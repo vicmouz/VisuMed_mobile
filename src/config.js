321df52d77efaf5d47a3297e7525009d75f208aa
Victor Mouzinho <jvictor2985@gmail.com>
2020-01-27 09:31:02 -0300

    Added a Procfile

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ global.ENCRYPT_KEY = "CECBD-AEA68877D2CC4-5A45692363993B5D";
 global.EMAIL_TEMPLATE = "Olá <strong>#</strong> seja bem vindo";
 
 module.exports = {
-  localDatabase: "mongodb://localhost:27017/VisuMed"
+  localDatabase:
+    "mongodb+srv://<username>:<password>@visumed-mjrci.mongodb.net/test?retryWrites=true&w=majority"
 };
