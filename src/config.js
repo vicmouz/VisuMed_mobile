c5f23694297fa32c5867b2281f2ecf92e7546fa2
Victor Mouzinho <jvictor2985@gmail.com>
2020-01-27 09:47:57 -0300

    Some modifications

--- src/config.js
+++ src/config.js
@@ -2,6 +2,13 @@ global.ENCRYPT_KEY = "CECBD-AEA68877D2CC4-5A45692363993B5D";
 global.EMAIL_TEMPLATE = "Olá <strong>#</strong> seja bem vindo";
 
 module.exports = {
-  localDatabase:
-    "mongodb+srv://<username>:<password>@visumed-mjrci.mongodb.net/test?retryWrites=true&w=majority"
+  mongo: {
+    development: {
+      connectionString: "mongodb://localhost:27017/VisuMed"
+    },
+    production: {
+      connectionString:
+        "postgres://nazffrfkyiwosm:bcce0998e98c84e6bd4ac8833a3f5e30820b9c135c9081056b2db18aaac9a770@ec2-52-71-122-102.compute-1.amazonaws.com:5432/dfk01fvl5484ah"
+    }
+  }
 };
