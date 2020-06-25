1cee8c7818117cb9444dde96d8cc3f4b970f8685
Victor Mouzinho <jvictor2985@gmail.com>
2020-06-25 19:02:15 -0300

    feat: base version

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'babel-plugin-root-import',
-      {
-        rootPathSuffix: 'src',
-      },
-    ],
-    [
-      'babel-plugin-inline-import',
-      {
-        extensions: ['.svg'],
-      },
-    ],
-  ],
 };
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import 'moment/locale/pt-br';
+/**
+ * @format
+ */
 import 'react-native-gesture-handler';
-// import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
-import { AppRegistry } from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './src';
-import { name as appName } from './app.json';
+import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
